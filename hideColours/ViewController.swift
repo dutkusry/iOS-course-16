17ec696777a2d20f58c9ac5eb26c6f2960be140d
Ryan Dutkus <RMD@ryans-macbook-pro.local>
2016-01-12 20:35:35 -0500

    Initial Commit

---
+++ hideColours/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hideColours
+//
+//  Created by Ryan Dutkus on 2016-01-12.
+//  Copyright © 2016 Ryan Dutkus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
